45ecd47c456f0adfa0da8a56a68a16b81caecb55
Aluno <Aluno@DESKTOP-AG93OEG>
2018-06-25 20:52:43 -0300

    quinta alteração

--- guilherme.php
+++ guilherme.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Guilherme</title>
-    </head>
-    <body>
-        <h1>Guilherme Medeiros de Souza</h1> <br>
-        esse ja deu aula pra nós!!
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
