b2123ea0c1e345899513008ded99828d2a99f32f
Aluno <Aluno@192.168.200.223>
2018-06-25 20:48:53 -0300

    AGORA VAI

--- sirio.php
+++ sirio.php
@@ -10,10 +10,12 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-         <h1>Leonardo Sirio</h1>
-        <a href="index.html">Voltar</a>
-        <br>
+         <h1>"Não há luz, nem esperança, há apenas desespero"
+                                             Madara Uchiha</h1>
+         <br>
         <img src="imagens/Madara.gif.gif" alt="madara">
+         <br>
+         <a href="index.html">Voltar</a>
                 <?php
         // put your code here
         ?>
