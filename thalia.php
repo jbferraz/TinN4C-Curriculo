cc396614e8e8eeb3f8b26fd844b397f9f599cb80
Aluno <Aluno@GRVL301B-18>
2018-06-25 20:04:46 -0300

    botão voltar

--- thalia.php
+++ thalia.php
@@ -10,6 +10,8 @@ and open the template in the editor.
         <title> qualquer coisa lá </title>
     </head>
     <body>
+        <h1> Thalia </h1>
+        <a href="index.html">voltar</a>
         <?php
         // put your code here
         ?>
